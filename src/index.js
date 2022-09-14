17ec7a664ad2ab3719c4045d003a5ed962dc97be
Lint Action <lint-action@samuelmeuli.com>
2022-09-14 12:14:31 +0000

    Fix code style issues with ESLint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css'
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,10 +15,10 @@ ReactDOM.render(
     </React.StrictMode>
   </BrowserRouter>,
 
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/network.js
+++ src/network.js
@@ -1,94 +1,87 @@
-import { useEffect, useState, useRef } from "react";
-import { Contract, ethers } from "ethers";
-import { createTheme, ThemeProvider } from "@material-ui/core";
-import { SnackbarProvider } from "notistack";
-import ConnectWalletPage from "./Components/connectWalletPage";
+import { createTheme } from '@material-ui/core'
+import { useEffect, useRef, useState } from 'react'
+
+import ConnectWalletPage from './Components/connectWalletPage'
+import * as chains from './constants/chains'
+import COINS from './constants/coins'
 import {
-  getProvider,
   getAccount,
   getFactory,
   getLongTermRouter,
+  getNetwork,
+  getProvider,
   getRouter,
   getSf,
-  getNetwork,
   getWeth,
-} from "./ethereumFunctions";
-import COINS from "./constants/coins";
-import * as chains from "./constants/chains";
+} from './ethereumFunctions'
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#ff0000",
-      contrastText: "#ffffff",
+      main: '#ff0000',
+      contrastText: '#ffffff',
     },
     secondary: {
-      main: "#9e9e9e",
-      contrastText: "#ffffff",
+      main: '#9e9e9e',
+      contrastText: '#ffffff',
     },
   },
-});
+})
 
-const autoReconnectDelay = 5000;
+const autoReconnectDelay = 5000
 
 const Web3Provider = (props) => {
-  const [isConnected, setConnected] = useState(true);
-  let network = Object.create({});
-  network.provider = useRef(null);
-  network.signer = useRef(null);
-  network.account = useRef(null);
-  network.coins = [];
-  network.chainID = useRef(null);
-  network.router = useRef(null);
-  network.longTermRouter = useRef(null);
-  network.factory = useRef(null);
-  network.weth = useRef(null);
-  const backgroundListener = useRef(null);
+  const [isConnected, setConnected] = useState(true)
+  let network = Object.create({})
+  network.provider = useRef(null)
+  network.signer = useRef(null)
+  network.account = useRef(null)
+  network.coins = []
+  network.chainID = useRef(null)
+  network.router = useRef(null)
+  network.longTermRouter = useRef(null)
+  network.factory = useRef(null)
+  network.weth = useRef(null)
+  const backgroundListener = useRef(null)
   async function setupConnection() {
     try {
-      console.log("lets go!");
-      network.provider = await getProvider();
-      network.signer = await network.provider.getSigner();
+      console.log('lets go!')
+      network.provider = await getProvider()
+      network.signer = await network.provider.getSigner()
       await getAccount().then(async (result) => {
-        network.account = result;
-      });
+        network.account = result
+      })
 
       await getNetwork(network.provider).then(async (chainId) => {
         // Set chainID
-        network.chainID = chainId;
+        network.chainID = chainId
         if (chains.networks.includes(chainId)) {
           // Get the router using the chainID
-          network.router = await getRouter(
-            chains.routerAddress.get(chainId),
-            network.signer
-          );
+          network.router = await getRouter(chains.routerAddress.get(chainId), network.signer)
           // Get the Superfluid router using the network
-          network.sf = await getSf(chainId);
+          network.sf = await getSf(chainId)
           // Get the deployed longTermSwap contract using the ChainID;
-          network.longTermRouter = await getLongTermRouter(
-            chains.longTermSwapAddress.get(chainId),
-            network.signer
-          );
+          network.longTermRouter = await getLongTermRouter(chains.longTermSwapAddress.get(chainId), network.signer)
           // Get default coins for network
-          network.coins = COINS.get(chainId);
+          network.coins = COINS.get(chainId)
           // Get Weth address from router
           await network.router.WETH().then((wethAddress) => {
-            network.weth = getWeth(wethAddress, network.signer);
+            network.weth = getWeth(wethAddress, network.signer)
             // Set the value of the weth address in the default coins array
-            network.coins[0].address = wethAddress;
-          });
+            network.coins[0].address = wethAddress
+          })
           // Get the factory address from the router
           await network.router.factory().then((factory_address) => {
-            network.factory = getFactory(factory_address, network.signer);
-          });
-          setConnected(true);
+            network.factory = getFactory(factory_address, network.signer)
+          })
+          setConnected(true)
         } else {
-          console.log("Wrong network mate.");
-          setConnected(false);
+          console.log('Wrong network mate.')
+          setConnected(false)
         }
-      });
+      })
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
   }
 
@@ -115,36 +108,36 @@ const Web3Provider = (props) => {
 
   useEffect(async () => {
     // Initial setup
-    console.log("Initial hook");
-    await setupConnection();
-    console.log("network: ", network);
+    console.log('Initial hook')
+    await setupConnection()
+    console.log('network: ', network)
 
     // Start background listener
     if (backgroundListener.current != null) {
-      clearInterval(backgroundListener.current);
+      clearInterval(backgroundListener.current)
     }
-    const listener = createListener();
-    backgroundListener.current = listener;
-    return () => clearInterval(backgroundListener.current);
-  }, []);
+    const listener = createListener()
+    backgroundListener.current = listener
+    return () => clearInterval(backgroundListener.current)
+  }, [])
 
   const renderNotConnected = () => {
-    console.log("Rendering");
+    console.log('Rendering')
     return (
       <div className="App">
         <div>
           <ConnectWalletPage />
         </div>
       </div>
-    );
-  };
+    )
+  }
 
   return (
     <>
       {!isConnected && renderNotConnected()}
       {isConnected && <div> {props.render(network)}</div>}
     </>
-  );
-};
+  )
+}
 
-export default Web3Provider;
+export default Web3Provider
